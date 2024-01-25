91e30cf518bf00eff10f5b18384522171437e3f8
TheSowut <sowutmail@gmail.com>
2024-01-25 23:54:44 +0200

    chore: remove JS files

--- scripts/enums/message.js
+++
@@ -1,4 +0,0 @@
-export var MESSAGE;
-(function (MESSAGE) {
-    MESSAGE["INVALID_ACCESS_TOKEN"] = "Invalid access token!";
-})(MESSAGE || (MESSAGE = {}));
--- scripts/enums/screen.js
+++
@@ -1,5 +0,0 @@
-export var SCREEN;
-(function (SCREEN) {
-    SCREEN[SCREEN["LOGIN"] = 0] = "LOGIN";
-    SCREEN[SCREEN["SHOWCASE"] = 1] = "SHOWCASE";
-})(SCREEN || (SCREEN = {}));
